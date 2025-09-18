9bfae498e6dd8856c98cac8ffee345db5474d1ff
NeilArmstrong <neil.armstrong@world.wide.weed>
2025-09-18 16:17:24 -0600

    ide-helper

--- src/Models/AuthProvider.php
+++ src/Models/AuthProvider.php
@@ -4,6 +4,13 @@ namespace Waterhole\Models;
 
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property-read \Waterhole\Models\User|null $user
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|AuthProvider newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|AuthProvider newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|AuthProvider query()
+ * @mixin \Eloquent
+ */
 class AuthProvider extends Model
 {
     const UPDATED_AT = null;
